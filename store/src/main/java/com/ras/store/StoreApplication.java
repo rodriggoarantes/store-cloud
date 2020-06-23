154eca3ae24b34db8b37e0d444cc786d65934c7f
rodrigo.arantes <rodrigo.arantes@totvs.com.br>
2020-06-23 00:05:40 -0300

    ajutes

--- store/src/main/java/com/ras/store/StoreApplication.java
+++ store/src/main/java/com/ras/store/StoreApplication.java
@@ -2,9 +2,20 @@ package com.ras.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class StoreApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class, args);
--- store/src/main/java/com/ras/store/application/fornecedor/FornecedorService.java
+++ store/src/main/java/com/ras/store/application/fornecedor/FornecedorService.java
@@ -1,6 +1,7 @@
 package com.ras.store.application.fornecedor;
 
 import com.ras.store.api.dto.FornecedorDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,25 +9,24 @@ import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import javax.swing.text.html.Option;
 import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class FornecedorService {
-    private static final String urlbase = "http://localhost:8181/api/suppliers";
+    private static final String urlbase = "http://fornecedor/api/suppliers";
 
-    private RestTemplate api;
+    private final RestTemplate api;
 
-    private RestTemplate api() {
-        if (Objects.isNull(api)) {
-            this.api = new RestTemplate();
-        }
-        return api;
+    @Autowired
+    public FornecedorService(RestTemplate restTemplate) {
+        this.api = restTemplate;
     }
 
+
+
     public Optional<FornecedorDto> obterFornecedor(@NonNull String estado) {
-        final ResponseEntity<FornecedorDto> fornecedorResponse = api()
+        final ResponseEntity<FornecedorDto> fornecedorResponse = this.api
                 .exchange(urlbase.concat("/states/").concat(estado),
                         HttpMethod.GET, null, FornecedorDto.class);
 
